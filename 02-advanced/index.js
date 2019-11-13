119dc676f47a72ade426c07e1fe18ae06b54d63a
Cao Yuhao <1174478646@qq.com>
2019-11-13 11:24:42 +0800

    add advanced socket demo

--- 02-advanced/index.js
+++ 02-advanced/index.js
@@ -1,6 +1,10 @@
-var app = require('express')()
+var express = require('express');
+var app = express()
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
@@ -17,6 +21,6 @@ io.on('connection', function (socket) {
     })
 })
 
-http.listen(3001, function () {
-    console.log('listening on *:3001')
+http.listen(3002, function () {
+    console.log('listening on *:3002')
 })
